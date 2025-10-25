ebc4bb4a7398e96e1286db8c6b5dd2007e8245ed
Abhijeet <abhijeetbhr8@gmail.com>
2025-10-25 14:03:09 +0530

    Deleted simple.cpp

--- simple.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-	cout<<"I am Abhijeet. I am getting myself familiar with this environment\n";
-	cout<<"I belong in the major leagues\n";
-	return 0;
-}
